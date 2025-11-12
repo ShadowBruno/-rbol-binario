c3cc0625e62ee65c2195b7a3f37bce2e2512d4cc
calocauri <calocauri@gmail.com>
2025-11-12 09:01:04 -0600

    agrego estructura de nodo

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+typedef struct node
+{
+    char value;
+    struct node *left;
+    struct node *right;
+} node;
+
+
+
 
 int main(void) {
     return 0;
+}
+
+
+void pre_order(struct node *n) {
+    
 }
