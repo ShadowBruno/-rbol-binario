570c37ffb5be1f7a9f1429839fe34157ef672120
Zmoon100 <fuentes.gutierrez.gael@gmail.com>
2025-11-13 09:58:19 -0600

    cambio 1 con node

--- In-order.c
+++ In-order.c
@@ -1,8 +1,25 @@
 //arbol binario in-order 
+//revisa si hay izquierda si hay cambia a izquierda si no imprime valor y luego revisa si hay derecha si si va a la izquierda y se repite 
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct Node {
+    char data;
+    struct Node* left;
+    struct Node* right;
+} Node; 
+
+
+
+
+
+
+
 int main() {
 printf("In-order Traversal of Binary Tree\n");
+
+
+
 
 return 0; 
 }
